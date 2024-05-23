fb28987bc752f501d41fef54bb9b17e7f9f27a8c
JuanBaut <deserd@protonmail.com>
2024-05-22 22:45:36 -0500

    reverting tlp limits when plugged

--- configuration.nix
+++ configuration.nix
@@ -124,14 +124,8 @@ in {
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-      RUNTIME_PM_ON_AC = "auto";
-      PCIE_ASPM_ON_AC = "powersupersave";
-      PLATFORM_PROFILE_ON_AC = "low-power";
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
-
       START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 
