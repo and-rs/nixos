7db84fb1a4c74ef2d1dcc98f6cfbdab3d00ac785
and-rs <deserd@protonmail.com>
2026-01-12 11:11:04 -0500

    yt-dlp wrapper

--- common/terminal.nix
+++ common/terminal.nix
@@ -1,5 +1,21 @@
 { pkgs, inputs, ... }: {
+  # yt-dlp setup to override my network block
   environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "yt-dlp-bypass" ''
+      set -e
+      REAL_RESOLV=$(${pkgs.coreutils}/bin/readlink -f /etc/resolv.conf)
+      DNS_FILE=$(mktemp)
+      echo "nameserver 1.1.1.1" > "$DNS_FILE"
+      echo "nameserver 8.8.8.8" >> "$DNS_FILE"
+      trap "rm -f $DNS_FILE" EXIT
+      echo "Bypassing DNS (Target: $REAL_RESOLV, Masking: nscd)..."
+      exec ${pkgs.bubblewrap}/bin/bwrap \
+        --dev-bind / / \
+        --bind "$DNS_FILE" "$REAL_RESOLV" \
+        --tmpfs /run/nscd \
+        -- ${pkgs.yt-dlp}/bin/yt-dlp "$@"
+    '')
+
     inputs.opencode.packages.${pkgs.stdenv.hostPlatform.system}.default
     imagemagick
     fastfetch
@@ -14,7 +30,6 @@
     unzip
     delta
     p7zip
-    helix
     kitty
     yazi
     btop
